aab29b2199f6b41bf724a45e2c79ee1b8c79fd0a
Kelverly Santos <kelverlysantos09@gmail.com>
2025-01-05 20:27:49 -0300

    att

--- script/menu.js
+++ script/menu.js
@@ -1,5 +1,15 @@
-const hamb = document.querySelector('.hamburger')
-const nav = document.querySelector('.nav')
+const hamb = document.querySelector('.hamburger'); 
+const nav = document.querySelector('.nav'); 
+const navLinks = document.querySelectorAll('.nav a'); 
 
-hamb.addEventListener('click', () =>
-     nav.classList.toggle('active'))
+
+hamb.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active'); 
+    });
+});
